f433c7df9e379891d8980bec4f263801f1086238
RenzoSC <renzoc009@gmail.com>
2022-06-17 14:59:12 -0300

    renzo

--- rpi-asm-framebuffer/app.s
+++ rpi-asm-framebuffer/app.s
@@ -14,12 +14,12 @@ main:
 	bl pintar_i
 	
 	
-	mov x1, 300
-	mov x2, 300
-	mov x3, 340
-	mov x4, 350
-	mov x5, 325
-	mov x6, 390
+	mov x1, 190
+	mov x2, 160
+	mov x3, 230
+	mov x4, 210
+	mov x5, 215
+	mov x6, 250
 	mov x7, 5
 	bl pintar_lampara
 InfLoop: 
